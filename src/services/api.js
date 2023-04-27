db354044849604e787699d19b2316494be0e5492
Manav Misra <manavm@visionwebsoft.com>
2023-04-27 06:52:53 -0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,54 @@
 import ky from "ky";
 
+const authAPI = ky.create({
+  hooks: {
+    //  https://github.com/sindresorhus/ky#hooksbeforeerror
+    beforeError: [
+      async (error) => {
+        /**
+         * If the request received a response,
+         * the error will be of type HTTPError and the Response object will be available at error.response.
+         * https://github.com/sindresorhus/ky#hooksbeforeretry
+         */
+        const { response } = error;
+        if (response && response.body) {
+          // Server sets `message` property on error.
+          const { message } = await response.json();
+          throw new Error(
+            `Authentication Error: ${message} (${response.status})`
+          );
+        }
+
+        throw error;
+      },
+    ],
+  },
+});
+
 export default {
   indexThoughts() {
-    return ky.get("http://localhost:3001/thoughts").json();
+    return ky.get(`http://localhost:3001/thoughts`).json();
+  },
+  async showThoughts(author) {
+    const allThoughts = await this.indexThoughts();
+    const thoughts4Author = allThoughts.filter(
+      (thought) => thought.author === author
+    );
+
+    if (!thoughts4Author.length) {
+      throw new Error("No thoughts found");
+    }
+
+    return thoughts4Author;
+  },
+  registerUser(newUser) {
+    return authAPI
+      .post(`http://localhost:3000/users/register`, { json: newUser })
+      .json();
+  },
+  loginUser(user) {
+    return authAPI
+      .post(`http://localhost:3000/users/login`, { json: user })
+      .json();
   },
 };
