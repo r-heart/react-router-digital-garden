941fafafd949f36382f455798d7a5047c1ceb676
Manav Misra <manavm@visionwebsoft.com>
2023-04-27 15:53:05 -0500

    Update text-input.jsx

--- src/components/form/text-input.jsx
+++ src/components/form/text-input.jsx
@@ -3,7 +3,13 @@ import capitalize from "lodash.capitalize";
 import startCase from "lodash.startcase";
 
 // Typically `id`s are kebab-case, but we want to use camelCase for the 'name' attribute.
-export default function TextInput({ id, label, placeholder, required = true, type = "text" }) {
+export default function TextInput({
+  id,
+  label,
+  placeholder,
+  required = true,
+  type = "text",
+}) {
   return (
     <div>
       <label className="sr-only" htmlFor={id}>
@@ -13,7 +19,7 @@ export default function TextInput({ id, label, placeholder, required = true, typ
         type={type}
         id={id}
         name={camelCase(id)}
-        placeholder={placeholder || startCase(id)}
+        placeholder={placeholder || label || startCase(id)}
         required={required}
       />
     </div>
