173ae1117c781ae8fce526501fb83ae60b8bda24
r-heart <rachaelkheart@gmail.com>
2023-10-10 17:28:20 -0700

    refactor: :recycle: refactor router in app.jsx to `createRoutesFromElements`

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,22 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Layout from "./components/layout";
 import { About, Contact, Home, LoginRegister } from "./pages";
 
-const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    children: [
-      { path: "", element: <Home /> },
-      { path: "about", element: <About /> },
-      { path: "contact", element: <Contact /> },
-      { path: "login", element: <LoginRegister /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path-="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="login" element={<LoginRegister />} />
+    </Route>
+  )
+);
 
 function App() {
   return <RouterProvider router={router} />;
