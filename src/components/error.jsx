bd3eda9a3f97d82ff87c13ff2742e29290268383
r-heart <rachaelkheart@gmail.com>
2023-10-10 18:07:03 -0700

    feat: :sparkles: create Error Component

---
+++ src/components/error.jsx
@@ -0,0 +1,17 @@
+import { Link, useAsyncError, useRouteError } from "react-router-dom";
+
+export default function Error() {
+  const asyncError = useAsyncError();
+  const routeError = useRouteError();
+
+  const error = asyncError || routeError;
+
+  return (
+    <p className="mt-8 rounded-md bg-red-100 px-4 py-2 text-red-700">
+      {error?.message + "!" || "An error occurred."}
+      <Link to="/" className="text-red-799 ml-4 hover:text-red-500">
+        Go Home 🏠. <div className=""></div>
+      </Link>
+    </p>
+  );
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 import { Await, useLoaderData } from "react-router-dom";
+import Error from "../components/error";
 import Loading from "../components/loading";
 import ThoughtList from "../components/thoughts/thought-list";
 
@@ -8,7 +9,7 @@ export default function Home() {
 
   return (
     <Suspense fallback={<Loading />}>
-      <Await resolve={thoughts}>
+      <Await resolve={thoughts} errorElement={<Error />}>
         <ThoughtList />
       </Await>
     </Suspense>
