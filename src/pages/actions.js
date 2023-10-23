2095376ea746a3322dc3fa9d07a6c12d57a2ac17
r-heart <rachaelkheart@gmail.com>
2023-10-23 13:01:29 -0700

    feat: :sparkles: update actions.js to get a JWT from the API service

--- src/pages/actions.js
+++ src/pages/actions.js
@@ -1,7 +1,12 @@
+import api from "../services/api";
+
 export const registerOrLogin = async ({ request }) => {
   const fd = await request.formData();
 
   const submittedUser = Object.fromEntries(fd);
 
   console.log(submittedUser);
+
+  const stuff = await api.registerUser(submittedUser);
+  console.log(stuff);
 };
