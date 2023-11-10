4b61195354da83dfe3cc70d6b033f54c2b5610dc
r-heart <rachaelkheart@gmail.com>
2023-11-10 11:25:31 -0800

    refactor: :recycle: use client-side validation to avoid a network request
    ensure any errors at user registration that don't require a network request aren't sending a network request

--- src/pages/actions.js
+++ src/pages/actions.js
@@ -1,12 +1,20 @@
 import { redirect } from "react-router-dom";
 import api from "../services/api";
-import { setTokenCookie } from "../services/utils";
+import {
+  setTokenCookie,
+  validateRegistrationPasswords,
+} from "../services/utils";
 
 export const registerOrLogin = async ({ request }) => {
   const fd = await request.formData();
   const submittedUser = Object.fromEntries(fd);
 
   try {
+    validateRegistrationPasswords(
+      submittedUser.password,
+      submittedUser.confirmedPassword
+    );
+
     const { token } = await api.registerUser(submittedUser);
 
     setTokenCookie(token);
