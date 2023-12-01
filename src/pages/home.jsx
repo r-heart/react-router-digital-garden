87db7e5807ca5cc84bfc48b1405c684d49558790
r-heart <rachaelkheart@gmail.com>
2023-12-01 14:44:34 -0800

    refactor: :recycle: implement useNavigation and custom hook for focusing, clearing and reseting thoughts form

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,35 +1,49 @@
 import { Suspense } from "react";
-import { Await, Form, useLoaderData, useOutletContext } from "react-router-dom";
+import {
+  Await,
+  Form,
+  useLoaderData,
+  useNavigation,
+  useOutletContext,
+} from "react-router-dom";
 import Error from "../components/error";
 import { TextInput } from "../components/form";
 import Loading from "../components/loading";
 import ThoughtList from "../components/thoughts/thought-list";
+import useClearAndFocus from "../hooks/use-clear-and-focus.js";
+import useError from "../hooks/use-error";
 import useSetCurrentUser from "../hooks/use-set-current-user";
 
 export default function Home() {
   const { thoughts } = useLoaderData();
+  const navigation = useNavigation();
   const [currentUser, setCurrentUser] = useOutletContext();
 
   useSetCurrentUser(setCurrentUser);
 
-  const handleRef = (node) => {
-    if (node) {
-      const input = node.elements.namedItem("thought");
-      if (input) {
-        input.focus();
-      }
-    }
-  };
+  const { error, isShowingError, setIsShowingError } = useError();
+
+  const isIdle = navigation.state === "idle";
+
+  const formRef = useClearAndFocus(error, isIdle);
 
   return (
     <>
       {currentUser && (
-        <Form method="post" ref={handleRef}>
+        <Form
+          method="POST"
+          onSubmit={() => {
+            setIsShowingError(false);
+          }}
+          ref={formRef}
+        >
           <TextInput
             id="thought"
             label="Add a New Thought"
             placeholder={`Hi ${currentUser}, what's on your mind 🧠 ?`}
           />
+
+          {error & isShowingError && <p className="error">{error}</p>}
           <button type="submit" className="btn">
             Add Thought
           </button>
